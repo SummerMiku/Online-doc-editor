5668e709e6abeca7695a7f06ce95c209667d720b
SummerMiku <211295692@qq.com>
2025-10-10 11:33:38 +0800

    改动3

--- Editor-FrontEnd/Editor-Doc-Editor/src/components/UserPage/LogRegister.vue
+++ Editor-FrontEnd/Editor-Doc-Editor/src/components/UserPage/LogRegister.vue
@@ -1,17 +1,42 @@
-<script setup>
+<script setup lang="ts">
 import { ref } from "vue";
+const isLogin = ref(true);
 function login() {
     console.log("login");
 }
 
+function register() {
+    console.log("register");
+}
+
 </script>
 <template>
-    <div class="">123123</div>
-    fdfdf
+    <div class="block-mask">
+        <div class="log-register">
+            <div class="log">
+                <div></div>
+                <div></div>
+            </div>
+            <div class="register"></div>
+        </div>
+    </div>
+
 </template>
 <style scoped>
-.log-register {
+.block-mask {
+    background-color: ragba(0, 0, 0, 0.5);
     width: 100%;
     height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 9999;
+}
+
+.log-register {
+    display: flex;
+    width: 70%;
+    height: 70%;
+    background-color: #c1c1c1;
 }
 </style>
